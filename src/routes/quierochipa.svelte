7c542d468fa908a91a5f400b4217a327ce0101e5
luxsan <luxcian9870@gmail.com>
2022-03-05 21:30:53 -0300

    'cualquiercosa'

---
+++ src/routes/quierochipa.svelte
@@ -0,0 +1,12 @@
+<div class="font-sans flex items-center justify-center bg-blue-darker w-full py-8">
+    <div class="overflow-hidden bg-white rounded max-w-xs w-full shadow-lg  leading-normal">
+        <a href="/alone" class="block group hover:bg-blue p-4 border-b">
+            <p class="font-bold text-lg mb-1 text-black group-hover:text-white">Create from scratch</p>
+            <p class="text-grey-darker mb-2 group-hover:text-white">Create a repository without any source code</p>
+        </a>
+        <a href="/otrosite" class="block group hover:bg-blue p-4">
+            <p class="font-bold text-lg mb-1 text-black group-hover:text-white">Fork</p>
+            <p class="text-grey-darker mb-2 group-hover:text-white">Start a project base on the source of an existing one.</p>
+        </a>
+    </div>
+</div>
